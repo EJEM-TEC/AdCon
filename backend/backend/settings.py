7bfd690377d847686f44d078e82b1ee3a173a31d
LucasZanini096 <lucas608@outlook.com>
2025-03-06 16:18:47 -0300

    feat: gunicorn config 9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rolepermissions',
-    'backend',
+    'backend.wsgi',
     'whitenoise.runserver_nostatic',
 ]
 
